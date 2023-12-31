282d3ed63248d6baf64300328c8676e8ca7b5d75
Haris <haris503@hotmail.com>
2023-12-30 22:52:06 -0500

    Add initial files

---
+++ script.js
@@ -0,0 +1,14 @@
+const ROWS = 16, COLS = 16;
+
+const grid = document.getElementById("grid");
+
+for (let y = 0; y < ROWS; y++) {
+    let row = document.createElement("row");
+    row.classList.add("row");
+    for (let x = 0; x < COLS; x++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        row.appendChild(square);
+    }
+    grid.appendChild(row);
+}
