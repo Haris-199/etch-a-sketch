b85c7f13d492560603de34d14fb5cabff988290c
Haris <haris503@hotmail.com>
2023-12-31 02:19:23 -0500

    create grid generation function

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-const ROWS = 16, COLS = 16;
 const grid = document.getElementById("grid");
 
-for (let y = 0; y < ROWS; y++) {
-    let row = document.createElement("row");
-    row.classList.add("row");
-    for (let x = 0; x < COLS; x++) {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        row.appendChild(square);
+function generateGrid(rows, cols) {
+    for (let y = 0; y < rows; y++) {
+        let row = document.createElement("row");
+        row.classList.add("row");
+        for (let x = 0; x < cols; x++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            row.appendChild(square);
+        }
+        grid.appendChild(row);
     }
-    grid.appendChild(row);
 }
+generateGrid(16, 16);
 
 grid.addEventListener("mouseover", event => {
-    // console.log(event.target.classList);
     if (event.target.classList.contains("square"))
         event.target.classList.add("filled");
 });
+
